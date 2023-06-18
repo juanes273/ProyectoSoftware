6fc9280cb60e875c21c16786c53b441216870e3a
Jose Becerra <jose.becerra@correounivalle.edu.co>
2023-06-17 22:32:00 -0500

    Pruebas unitarias

--- cliente/src/__test__/Form.test.js
+++ cliente/src/__test__/Form.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
-import Form from '../components/Form';
+import Form from '../components/form';
 import '@testing-library/jest-dom'
 import '@testing-library/jest-dom'
 
--- cliente/src/__test__/ListGroup.test.js
+++ cliente/src/__test__/ListGroup.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import ListGroup from '../components/ListGroup';
+import ListGroup from '../components/listGroup';
 import '@testing-library/jest-dom';
 
 describe('ListGroup Component', () => {
